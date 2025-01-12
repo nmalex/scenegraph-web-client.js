2bf2cfe0cc682ed1cb3ccd3aa959bc087c5b075e
nmalex <horoshiloff@gmail.com>
2025-01-12 13:19:20 +0100

    link three.js directories

---
+++ public/create-links.bat
@@ -0,0 +1,4 @@
+mklink /D "build"    "C:\Projects\scenegraph-three.js\build"
+mklink /D "editor"   "C:\Projects\scenegraph-three.js\editor"
+mklink /D "examples" "C:\Projects\scenegraph-three.js\examples"
+mklink /D "files"    "C:\Projects\scenegraph-three.js\files"
